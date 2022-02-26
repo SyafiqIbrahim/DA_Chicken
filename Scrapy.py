197fe46d53b7c002e21d56742c1ad8d277abf30d
SyafiqIbrahim <syafiqboi@gmail.com>
2022-02-26 20:50:18 +0800

    Version1

--- Scrapy.py
+++ Scrapy.py
@@ -13,22 +13,33 @@ class Extract(scrapy.Spider):
     #Website input
     start_urls = ['http://192.168.182.143/spicyx']
 
+    ####################################
+    #Parse function
+    #Start crawling website
+    ####################################
     def parse(self, response):
         #Extract image with 'img' tag
         Image_Selector = 'img'
 
+        #Loop response to extract each image URL
         for x in response.css(Image_Selector):
-            #Image stored in src attribute
-            newsel = '@src'
+            #Image URL stored in src attribute
+            URL = '@src'
 
-            #Display image url
+            #Display image URL
             yield {
-                'Image Link:': x.xpath(newsel).extract_first(),
+                'Image Link:': x.xpath(URL).extract_first(),
             }
-
-        # To recurse next page
+        ###########################
+        #To recurse next page
+        ###########################
+        #Select '.next a' tag with 'href' attribute
         Page_Selector = '.next a ::attr(href)'
+
+        #Extract URL to next page
         next_page = response.css(Page_Selector).extract_first()
+
+        #If valid URL is available, follow link and call Parse function on the link
         if next_page:
             yield scrapy.Request (
                 response.urljoin(next_page),
